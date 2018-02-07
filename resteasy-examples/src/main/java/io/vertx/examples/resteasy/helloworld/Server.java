1377219257fba162f063b5c0023b2f3082b63c77
syd <numbcode@gmail.com>
2018-02-08 01:22:31 +0800

    execute blocking

--- resteasy-examples/src/main/java/io/vertx/examples/resteasy/helloworld/Server.java
+++ resteasy-examples/src/main/java/io/vertx/examples/resteasy/helloworld/Server.java
@@ -19,22 +19,27 @@ public class Server extends AbstractVerticle {
   }
 
   @Override
-  public void start() throws Exception {
-
-    // Build the Jax-RS hello world deployment
-    VertxResteasyDeployment deployment = new VertxResteasyDeployment();
-    deployment.start();
-    deployment.getRegistry().addPerInstanceResource(HelloWorldService.class);
-
-    Router router = Router.router(vertx);
-    router.route().handler(RestEasyHandler.create(vertx, deployment));
-
-    // Start the front end server using the Jax-RS controller
-    vertx.createHttpServer()
-      .requestHandler(router::accept)
-      .listen(8080, ar -> {
-        System.out.println("Server started on port " + ar.result().actualPort());
-      });
+  public void start(Future<Void> startFuture) throws Exception {
+    vertx.executeBlocking(future -> {
+      //初始化的时候有点慢，用executeBlocking执行
+      // Build the Jax-RS hello world deployment
+      VertxResteasyDeployment deployment = new VertxResteasyDeployment();
+      deployment.start();
+      deployment.getRegistry().addPerInstanceResource(HelloWorldService.class);
+      Router router = Router.router(vertx);
+      router.route().handler(RestEasyHandler.create(vertx, deployment));
+      // Start the front end server using the Jax-RS controller
+      vertx.createHttpServer()
+        .requestHandler(router::accept)
+        .listen(8080, ar -> {
+          if (ar.succeeded()) {
+            System.out.println("Server started on port " + ar.result().actualPort());
+            startFuture.complete();
+          } else {
+            startFuture.fail(ar.cause());
+          }
+        });
+    }, null);
 
   }
 }
